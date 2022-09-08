1bd42a7bb089a3d9555b131fe4d33db71d06d04d
Stephane Berube <sirber@hotmail.com>
2022-09-07 20:36:50 -0400

    working

--- backup.ps1
+++ backup.ps1
@@ -1,3 +1,13 @@
-# TODO: read settings.json
+if (-not (Test-Path -Path .\settings.json -PathType Leaf)) {
+    Write-Host "settings.json not found"
+    exit 1
+}
 
-# TODO: loop and zip
+$sources = Get-Content .\settings.json -Raw | ConvertFrom-Json 
+
+ForEach ($source in $sources) {
+    Write-Host "Compressing $($source.name)..."
+    Compress-Archive -Path $source.path -DestinationPath $source.outputZip -Force
+}
+
+Write-Host "Done!"
